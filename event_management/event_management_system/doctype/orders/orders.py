5a6a9c30236165b16321a851bbe7cd003a7120e7
raffalfian <alfian@tandigital.id>
2024-06-22 07:34:50 +0700

    feat(orders): add doctype and validation orders

--- event_management/event_management_system/doctype/orders/orders.py
+++ event_management/event_management_system/doctype/orders/orders.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2024, Noob Team and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Orders(Document):
-	pass
+    def validate(self):
+        self.calculate_total_price()
+
+    def calculate_total_price(self):
+        if self.events and self.total_ticket:
+            event_price = frappe.db.get_value('Events', self.events, 'price')
+            self.total_price = self.total_ticket * event_price
+
+    def before_save(self):
+        self.calculate_total_price()
