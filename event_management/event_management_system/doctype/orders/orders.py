c43785587c8f4d2aca48b853b67c756c258a2280
MDafiR <dafi@tandigital.id>
2024-06-25 09:23:49 +0700

    feat(orders): add calculate and checking availability for event ticket

--- event_management/event_management_system/doctype/orders/orders.py
+++ event_management/event_management_system/doctype/orders/orders.py
@@ -2,17 +2,44 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class Orders(Document):
     def validate(self):
+        self.check_ticket_availability()
+    
+    def before_submit(self):
         self.calculate_total_price()
+        self.reduce_event_tickets()
+    
+    def before_cancel(self):
+        self.return_event_tickets()
+
+    def check_ticket_availability(self):
+        if self.event_id and self.total_ticket:
+            available_tickets = frappe.db.get_value('Events', self.event_id, 'number_of_tickets')
+            # Cek apakah jumlah tiket yang diminta melebihi jumlah yang tersedia
+            if self.total_ticket > available_tickets:
+                frappe.throw(_("Not enough tickets available for the event. Only {0} tickets remaining.")
+                             .format(available_tickets))
 
     def calculate_total_price(self):
-        if self.events and self.total_ticket:
-            event_price = frappe.db.get_value('Events', self.events, 'price')
+        if self.event_id and self.total_ticket:
+            event_price = frappe.db.get_value('Events', self.event_id, 'price')
             self.total_price = self.total_ticket * event_price
-
-    def before_save(self):
-        self.calculate_total_price()
+    
+    def reduce_event_tickets(self):
+        if self.event_id and self.total_ticket:
+            event = frappe.get_doc('Events', self.event_id)
+            # Kurangi jumlah tiket yang tersedia
+            event.number_of_tickets -= self.total_ticket
+            event.save()
+    
+    def return_event_tickets(self):
+        if self.event_id and self.total_ticket:
+            event = frappe.get_doc('Events', self.event_id)
+            # Tambahkan kembali jumlah tiket yang tersedia
+            event.number_of_tickets += self.total_ticket
+            event.save()
